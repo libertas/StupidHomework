a16acfdfe3706e73cd030dcb115aace9dbf9ef5a
libertas <horizonvei@gmail.com>
2015-11-13 19:37:22 +0800

    complete

--- 2015.11.11/Task2/src/Task2.java
+++ 2015.11.11/Task2/src/Task2.java
@@ -4,13 +4,24 @@ public class Task2 {
 	public static void breakMoney(int f, int t, int o) {
 		total++;
 		System.out.printf("5*%d,\t2*%d,\t1*%d.\tTotal:%d Yuan.\n", f, t, o, f*5+t*2+o*1);
+			
+		while(o > 1) {
+			total++;
+			t++;
+			o -= 2;
+			System.out.printf("5*%d,\t2*%d,\t1*%d.\tTotal:%d Yuan.\n", f, t, o, f*5+t*2+o*1);
+		}
 		
-		if(f != 0) {
+		while(t > 0) {
+			t--;
+			o += 2;
+		}
+		
+		if(f > 0) {
 			f--;
-			breakMoney(f, t + 2, o + 1);
-			breakMoney(f, t + 1, o + 3);
 			breakMoney(f, t, o + 5);
 		}
+
 	}
 	
 	public static void findMoney(int money) {
@@ -19,13 +30,14 @@ public class Task2 {
 		int twos = complement / 2, ones = complement % 2;
 		
 		total = 0;
-		
-		do {
-			breakMoney(fives, twos, ones);
+
+		while(twos > 0) {
 			twos--;
 			ones += 2;
-		} while(twos >= 0);
+		}
 		
+		breakMoney(fives, twos, ones);
+
 		System.out.printf("Total probabilities:%d\n", total);
 	}
 	
