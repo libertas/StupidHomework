e11eda446952b18c9c333085fdd7e52e3717cb31
libertas <horizonvei@gmail.com>
2015-12-07 23:33:47 +0800

    add list

--- 2015.12.6/task2.c
+++ 2015.12.6/task2.c
@@ -7,21 +7,55 @@ struct lnode
     struct lnode *next;
 };
 
-int main()
+int list_create(struct lnode *head, int *d, int length)
 {
     int i;
-    int user_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9 ,10};
-    struct lnode H;
     struct lnode *p;
 
-    for(i = 0; i < 10; i++) {
-        struct lnode *p = malloc(sizeof(struct lnode));
-        if(p == NULL)
+    p = head;
+
+    for(i = 0; i < length; i++) {
+        p->next = calloc(1, sizeof(struct lnode));
+        if(p->next == NULL)
             return -1;
 
-        p->data = user_data[i];
-
+        p = p ->next;
+        p->data = *(d + i);
+        p->next = NULL;
     }
+
+    return 0;
+}
+
+int list_delete(struct lnode *head, int d)
+{
+
+    return 0;
+}
+
+int print_list(struct lnode *p)
+{
+    while(p->next != NULL)
+    {
+        p = p->next;
+        printf("%d\n", p->data);
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int user_data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9 ,10};
+    struct lnode *h = calloc(1, sizeof(struct lnode));;
+
+    if(0 != list_create(h, user_data, 10))
+    {
+        printf("Error: cannot allocate memory!\n");
+        return -1;
+    }
+
+    print_list(h);
 
     return 0;
 }
