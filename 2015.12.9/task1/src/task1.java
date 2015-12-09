5117cf17f92ff5d530544e9497ab4f9894590572
libertas <horizonvei@gmail.com>
2015-12-09 16:07:31 +0800

    task1 complete

--- 2015.12.9/task1/src/task1.java
+++ 2015.12.9/task1/src/task1.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class task1 {
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Please input a string:");
+		
+		String str = sc.nextLine();
+		sc.close();
+		
+		while(0 != str.length()) {
+			String c = str.substring(0, 1);
+			String tmp = str;
+			str = str.replace(c, "");
+			System.out.printf("%s: %d times", c, tmp.length() - str.length());
+			System.out.println("");
+		}
 	}
 }
