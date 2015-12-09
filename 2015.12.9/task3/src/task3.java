578c0b9297abdb8ac37b89168dcd9b77966b7019
libertas <horizonvei@gmail.com>
2015-12-09 21:08:46 +0800

    task3 complete

--- 2015.12.9/task3/src/task3.java
+++ 2015.12.9/task3/src/task3.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class task3 {
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Please input an ip address:");
+		
+		String str = sc.nextLine();
+		sc.close();
+		
+		System.out.print(str.matches(
+				"^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}|[1-9]{1}[0-9]{1}|[1-9])\\.){3}"
+				+ "((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}|[1-9]{1}[0-9]{1}|[1-9]))$"
+						)
+				);
 	}
 }
