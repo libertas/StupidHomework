776584f502a1ae8d15dee4fa804611a7af805f79
libertas <horizonvei@gmail.com>
2015-12-09 17:03:16 +0800

    task2 complete

--- 2015.12.9/task2/src/task2.java
+++ 2015.12.9/task2/src/task2.java
@@ -1,6 +1,45 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 
 public class task2 {
+	private static void PrintDate(String dates) {
+		try  
+		{
+		    SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+		    Date date = sdf.parse(dates);
+		    
+		    dates =(new SimpleDateFormat("MMMM dd,yyyy")).format(date);
+		    System.out.println(dates);
+		}
+		catch (ParseException e)
+		{
+			try  
+			{
+			    SimpleDateFormat sdf = new SimpleDateFormat("MMMM dd,yyyy");
+			    Date date = sdf.parse(dates);
+			    
+			    dates=(new SimpleDateFormat("yyyy.MM.dd")).format(date);
+			    System.out.println(dates);
+			}
+			catch (ParseException e1)
+			{
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+	
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please input a date:");
+		
+		String d = sc.nextLine();
+		sc.close();
+		
+		PrintDate(d);
 		
 	}
 }
