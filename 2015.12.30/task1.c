eb519f26e0fce96e124069a5e2f2c5d2219ef4e2
libertas <horizonvei@gmail.com>
2015-12-30 16:30:55 +0800

    add task1,buggy

--- 2015.12.30/task1.c
+++ 2015.12.30/task1.c
@@ -1,6 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct bitnode
+{
+    char data;
+    struct bitnode *lc, *rc;
+} BiTNode, *BiTree;
+
+BiTree init()
+{
+    BiTNode *bt;
+    bt = (BiTNode *) malloc(sizeof(BiTNode));
+    bt->lc = NULL;
+    bt->rc = NULL;
+    return bt;
+}
+
+char create(BiTree bt)
+{
+    char c;
+
+    c = getchar();
+
+    if('\n' == c) {
+        return '\n';
+    } else if('*' == c) {
+        return '*';
+    } else {
+        bt->data = c;
+    }
+
+    bt->lc = malloc(sizeof(BiTNode));
+    c = create(bt->lc);
+
+    if('\n' == c) {
+        return '\n';
+    } else if('*' == c) {
+        free(bt->lc);
+        bt->lc = NULL;
+    } else {
+        bt->data = c;
+    }
+
+    bt->rc = malloc(sizeof(BiTNode));
+    c = create(bt->rc);
+
+    if('\n' == c) {
+        return '\n';
+    } else if('*' == c) {
+        free(bt->lc);
+        bt->lc = NULL;
+    } else {
+        bt->data = c;
+    }
+
+    return 0;
+}
+
+int pre_order(BiTree bt)
+{
+    printf("%c\n", bt->data);
+    if(NULL != bt->lc)
+        pre_order(bt->lc);
+    if(NULL != bt->rc)
+        pre_order(bt->rc);
+    return 0;
+}
 
 int main()
 {
+    BiTree t = init();
+    create(t);
+    pre_order(t);
     return 0;
 }
