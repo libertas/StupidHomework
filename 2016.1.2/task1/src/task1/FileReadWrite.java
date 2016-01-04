81fdfde138ecfd11fc89435ece1f90bac0803ea8
libertas <horizonvei@gmail.com>
2016-01-04 23:34:12 +0800

    task1 complete

--- 2016.1.2/task1/src/task1/FileReadWrite.java
+++ 2016.1.2/task1/src/task1/FileReadWrite.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class FileReadWrite {
@@ -34,5 +36,23 @@ public class FileReadWrite {
 		
 		pw.print(data);
 		pw.close();
+		
+		String[] data_list = data.split("[;:,. \"!?]");
+		ArrayList<String> al = new ArrayList<String>();
+		
+		System.out.println("Number of the words:");
+		System.out.println(data_list.length);
+		
+		for(String s: data_list) {
+			al.add(s);
+		}
+		
+		while(al.size() != 0) {
+			String tmp = al.get(0);
+			System.out.println(tmp + ":" + Collections.frequency(al, tmp));
+			while(Collections.frequency(al, tmp) != 0) {
+				al.remove(tmp);
+			}
+		}
 	}
 }
